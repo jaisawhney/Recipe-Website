567b4961a53be7f339aadaed8dcaab1ef7b23109
Jai Sawhney <jai.sawhney1@gmail.com>
2021-12-07 22:45:23 -0800

    Add mock data and update images

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 import os
+import json
+
+file = open("mock-data.json")
+mock_data = json.load(file)
 
 app = Flask(__name__)
 
@@ -11,12 +15,24 @@ def index():
 
 @app.route("/categories")
 def show_categories():
-    return render_template("categories.html")
+    categories = mock_data["categories"]
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/recipes")
 def show_recipes():
-    return render_template("recipes.html")
+    category = request.args.get("category") or None
+    recipes = mock_data["recipes"]
+    return render_template("recipes.html", recipes=recipes, category=category, categories=mock_data["categories"])
+
+
+@app.route("/recipes/<string:recipe_id>")
+def show_recipe(recipe_id):
+    recipe = next(recipe for recipe in mock_data["recipes"] if recipe["id"] == recipe_id)
+    if recipe:
+        return render_template("recipe.html", recipe=recipe)
+    else:
+        return redirect(url_for("show_recipes"))
 
 
 if __name__ == "__main__":
